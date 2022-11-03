215bbc37c280513acd498a45fc5073d1269a141f
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2022-11-03 12:14:43 +0300

    feat: создать класс Option (гл. 6)

---
+++ src/core/option/Option.spec.ts
@@ -0,0 +1,99 @@
+import { assert } from 'chai'
+import Option from './Option'
+
+describe('Тест класса Option', () => {
+    const nan: Factory<typeof NaN> = () => NaN
+
+    describe('Тест конструктора new', () => {
+        describe('Аргумент конструктора не nullable', () => {
+            const zero = Option.new(0)
+
+            it('Экземпляр класса Option.Some', () => {
+                assert.instanceOf(zero, Option.Some)
+            })
+            it('Свойство isEmpty - false', () => {
+                assert.isFalse(zero.isEmpty)
+            })
+        })
+
+        describe('Аргумент конструктора nullable', () => {
+            const [NULL, UNDEF, NONE] = [Option.new(null), Option.new(undefined), Option.new()]
+
+            it('Экземпляр класса Option.Empty - синглтон', () => {
+                // @ts-expect-error
+                assert.isTrue(NULL === UNDEF && UNDEF === NONE)
+            })
+            it('Свойство isEmpty - true', () => {
+                assert.isTrue(Option.new().isEmpty)
+            })
+        })
+    })
+
+    describe('Тест метода getOrElse', () => {
+        it('Значение отсутствует', () => {
+            assert.isNaN(Option.new<number>().getOrElse(nan))
+        })
+        it('Значение присутствует', () => {
+            assert.equal(Option.new<number>(0).getOrElse(nan), 0)
+        })
+    })
+
+    describe('Тест метода isNotEmpty', () => {
+        it('Значение отсутствует', () => {
+            assert.isFalse(Option.new<number>().isNotEmpty())
+        })
+        it('Значение присутствует', () => {
+            assert.isTrue(Option.new<number>(0).isNotEmpty())
+        })
+    })
+
+    describe('Тест метода map', () => {
+        const add = (n: number): number => n + 1
+
+        it('Значение отсутствует', () => {
+            assert.isNaN(Option.new<number>().map(add).getOrElse(nan))
+        })
+        it('Значение присутствует', () => {
+            assert.equal(Option.new<number>(0).map(add).getOrElse(nan), 1)
+        })
+    })
+
+    describe('Тест метода flatMap', () => {
+        const add = (n: number): Option<number> => Option.new(n + 1)
+
+        it('Значение отсутствует', () => {
+            assert.isNaN(Option.new<number>().flatMap(add).getOrElse(nan))
+        })
+        it('Значение присутствует', () => {
+            assert.equal(Option.new<number>(0).flatMap(add).getOrElse(nan), 1)
+        })
+    })
+
+    describe('Тест метода filter', () => {
+        const isZero: Predicate<number> = (n) => n === 0
+
+        it('Значение отсутствует', () => {
+            assert.isNaN(Option.new<number>().filter(isZero).getOrElse(nan))
+        })
+        it('Значение присутствует - оригинальный экземпляр', () => {
+            const zeroA = Option.new<number>(0)
+            const zeroB = zeroA.filter(isZero)
+
+            assert.equal(zeroA, zeroB)
+        })
+        it('Значение присутствует - 0', () => {
+            assert.equal(Option.new<number>(0).filter(isZero).getOrElse(nan), 0)
+        })
+    })
+
+    describe('Тест метода orElse', () => {
+        const minusOne: Factory<Option<-1>> = () => Option.new(-1)
+
+        it('Значение отсутствует', () => {
+            assert.equal(Option.new<number>().orElse(minusOne).getOrElse(nan), -1)
+        })
+        it('Значение присутствует', () => {
+            assert.equal(Option.new<number>(0).orElse(minusOne).getOrElse(nan), 0)
+        })
+    })
+})
---
+++ src/core/option/Option.ts
@@ -0,0 +1,69 @@
+import { singleton } from 'src/utils/decorators'
+
+abstract class Option<T> {
+    static new<T>(value: T | null = null): Option<T> {
+        return value === null ? Option.None.instance() : new Option.Some(value)
+    }
+
+    filter(p: Predicate<T>): Option<T> {
+        return this.flatMap((value) => (p(value) ? this : Option.new()))
+    }
+
+    flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+        return this.map(f).getOrElse(() => Option.new())
+    }
+
+    isNotEmpty(): this is Option.Some<T> {
+        return !this.isEmpty
+    }
+
+    orElse(f: Factory<Option<T>>): Option<T> {
+        return this.map(() => this as Option<T>).getOrElse(f)
+    }
+
+    abstract get isEmpty(): boolean
+    abstract getOrElse(f: Factory<T>): T
+    abstract map<U>(f: (value: T) => U): Option<U>
+}
+
+namespace Option {
+    export class Some<T> extends Option<T> {
+        readonly #value: T
+
+        constructor(value: T) {
+            super()
+            this.#value = value
+        }
+
+        get isEmpty(): boolean {
+            return false
+        }
+
+        getOrElse(f: Factory<T>): T {
+            return this.#value
+        }
+
+        map<U>(f: (value: T) => U): Option<U> {
+            return new Some(f(this.#value))
+        }
+    }
+
+    @singleton
+    export class None extends Option<never> {
+        static instance: () => None
+
+        get isEmpty(): boolean {
+            return true
+        }
+
+        getOrElse<T>(f: Factory<T>): T {
+            return f()
+        }
+
+        map<U>(f: (value: never) => U): Option<U> {
+            return this
+        }
+    }
+}
+
+export default Option
